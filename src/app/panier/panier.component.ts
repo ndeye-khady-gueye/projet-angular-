ee03247c0a42998fedd116f5fa77e695e7a660e7
lolo <ndeyekhadygueye414@gmail.com>
2025-03-18 08:14:16 +0000

    Notre projet

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,34 +1,38 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {ProductsService} from "../products/data-access/products.service";
-import {NgForOf} from "@angular/common";
-import {Router} from "@angular/router";
-import {of} from "rxjs";
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from "../products/data-access/products.service";
+import { NgForOf, NgIf } from "@angular/common";
+import { Router } from "@angular/router";
+import { PaginatorModule } from "primeng/paginator";
+import { TableModule } from "primeng/table";
 
 @Component({
   selector: 'app-panier',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf,
+    PaginatorModule,  // Ajout du module de pagination
+    TableModule       // Ajout du module de tableau PrimeNG
   ],
   templateUrl: './panier.component.html',
   styleUrl: './panier.component.css'
 })
 export class PanierComponent implements OnInit {
-  ProductsService : ProductsService = inject(ProductsService);
-  card! : any
-
-ngOnInit () {
-   this.card = this.ProductsService.cart
-   this.ProductsService.getCart()
-   
-}
-  constructor(private router: Router) {
-  }
-
-
-deleteproduct(id:number){
-  this.ProductsService.removeFromCart(id)
+  ProductsService: ProductsService = inject(ProductsService);
+  card!: any;
   
-}
+  // Variables pour la pagination
+  page: number = 0;
+  rows: number = 5; // Nombre d'éléments par page
 
+  ngOnInit() {
+    this.card = this.ProductsService.cart;
+    this.ProductsService.getCart();
+  }
+
+  constructor(private router: Router) { }
+
+  deleteproduct(id: number) {
+    this.ProductsService.removeFromCart(id);
+  }
 }
